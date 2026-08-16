b5c7a50638c8e98042a02c6f1a14452895c84277
Irakli Gharibashvili <iraklisan@gmail.com>
2026-08-16 18:46:59 +0400

    Convert Selection shortcuts for C4D
    Convert selected Points, Edges or Polygons into a different component selection mode.

---
+++ Convert_Sel_2_Polys.py
@@ -0,0 +1,12 @@
+import c4d
+
+bc = c4d.BaseContainer()
+
+bc[c4d.MDATA_CONVERTSELECTION_LEFT] = 5
+bc[c4d.MDATA_CONVERTSELECTION_RIGHT] = 2
+
+c4d.utils.SendModelingCommand(command = c4d.MCOMMAND_CONVERTSELECTION, list = [op], mode = 2, bc = bc, doc = doc)
+
+c4d.CallCommand(12187)
+
+c4d.EventAdd()
